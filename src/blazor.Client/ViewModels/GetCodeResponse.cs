dbdd870350c31c98533049719f18a6a14255e05d
majikandy <github@andy-davis.co.uk>
2018-06-28 17:26:51 +0100

    Generic storage values for any contract

---
+++ src/blazor.Client/ViewModels/GetCodeResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace blazor.Client.ViewModels
+{
+    public class GetCodeResponse
+    {
+        public string bytecode { get; set; }
+
+        public string csharp { get; set; }
+
+        public string message { get; set; }
+    }
+}
