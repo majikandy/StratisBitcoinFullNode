70b08fd8dbfc80f8ee24ee8249a66ef1a2a030d3
majikandy <github@andy-davis.co.uk>
2018-10-01 20:51:25 +0100

    sphere v1

---
+++ src/SphereD/Program.cs
@@ -0,0 +1,50 @@
+﻿using NBitcoin;
+
+namespace SphereD
+{
+    using System;
+    using System.Threading.Tasks;
+    using NBitcoin.Protocol;
+    using Stratis.Bitcoin;
+    using Stratis.Bitcoin.Builder;
+    using Stratis.Bitcoin.Configuration;
+    using Stratis.Bitcoin.Features.Api;
+    using Stratis.Bitcoin.Features.Apps;
+    using Stratis.Bitcoin.Features.BlockStore;
+    using Stratis.Bitcoin.Features.Consensus;
+    using Stratis.Bitcoin.Features.MemoryPool;
+    using Stratis.Bitcoin.Features.Miner;
+    using Stratis.Bitcoin.Features.RPC;
+    using Stratis.Bitcoin.Features.Wallet;
+    using Stratis.Bitcoin.Utilities;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var nodeSettings = new NodeSettings(protocolVersion: ProtocolVersion.ALT_PROTOCOL_VERSION, args: args, network: new SphereMainNetwork());
+
+                IFullNode node = new FullNodeBuilder()
+                    .UseNodeSettings(nodeSettings)
+                    .UseBlockStore()
+                    .UsePosConsensus()
+                    .UseMempool()
+                    .UseWallet()
+                    .AddPowPosMining()
+                    .UseApi()
+                    .UseApps()
+                    .AddRPC()
+                    .Build();
+
+                if (node != null)
+                    node.RunAsync().GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem initializing the node. Details: '{0}'", ex.ToString());
+            }
+        }
+    }
+}
